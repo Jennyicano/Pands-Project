cc982e83511e411bda1b9394f1638e2dfc43dfb6
Jenny Ibanez <jenny.ibanezcamo@gmail.com>
2024-05-01 17:00:22 +0100

    loading data cvs

--- Analysis.py
+++ Analysis.py
@@ -1,10 +1,5 @@
 # Analysis of the Iris Data Set. 
 # Author Jennifer Ibanez Cano
 
-# https://archive.ics.uci.edu/dataset/53/iris
-#https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv
-
-import csv
-import pandas as pd
-
-df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+import os
+import numpy as np
