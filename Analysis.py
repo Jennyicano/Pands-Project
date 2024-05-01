564b0020243bc56608a9db60f1e735daf0822956
Jenny Ibanez <jenny.ibanezcamo@gmail.com>
2024-05-01 11:25:28 +0100

    trying to load the data set

---
+++ Analysis.py
@@ -0,0 +1,12 @@
+# Analysis of the Iris Data Set. 
+# Author Jennifer Ibanez Cano
+
+# https://archive.ics.uci.edu/dataset/53/iris
+
+import csv
+
+filename = csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv")
+
+filename = "iris_data.txt"
+
+print(filename)
