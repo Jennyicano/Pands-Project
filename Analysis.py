ef2fb7672e9dab478e222372c42362526093f2dd
Jenny Ibanez <jenny.ibanezcamo@gmail.com>
2024-05-02 11:12:41 +0100

    loading data txt with pandas

--- Analysis.py
+++ Analysis.py
@@ -1,21 +1,15 @@
 # Analysis of the Iris Data Set. 
 # Author Jennifer Ibanez Cano
 
-import os
+
 import numpy as np
-import csv
+import pandas as pd
 
-# Load the data from txt to csv
+# Loaad the data
+df = pd.read_csv('iris.data.txt')
 
-with open('iris.data.txt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[4]} species, {row[0]} sepal_length, {row[1]} sepal_width, {row[2]} petal_length, {row[3]} petal_width')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
-    
+# Check the data
+print(df)
+
+
+
